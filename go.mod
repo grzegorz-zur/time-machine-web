9723dc81c9a75e078f90952d4f2c74cdfdd1f7fa
Grzegorz Żur <grzegorz.zur@gmail.com>
2018-11-17 23:17:49 +0100

    Get current value

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/grzegorz-zur/time-machine-web
--- main.go
+++ main.go
@@ -6,33 +6,56 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 type TimeMachine struct {
-	PID string
+	Pid   string
+	Value int64
 }
 
 var (
-	tmDir     = regexp.MustCompile("timemachine-([0-9]+)")
-	indexTmpl = template.Must(template.ParseFiles("index.html"))
+	pattern = regexp.MustCompile("timemachine-([0-9]+)")
+	tms     = template.Must(template.ParseFiles("tms.html"))
+	tm      = template.Must(template.ParseFiles("tm.html"))
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/", handler)
 	err := http.ListenAndServe(":9876", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	tms := find()
-	indexTmpl.Execute(w, tms)
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.URL.Path == "/":
+		listHandler(w, r)
+	case r.Method == "GET":
+		getHandler(w, r)
+	case r.Method == "POST":
+		setHandler(w, r)
+	}
 }
 
-func find() (tms []TimeMachine) {
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	tms.Execute(w, list())
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	pid := strings.Trim(r.URL.Path, "/")
+	tm.Execute(w, get(pid))
+}
+
+func setHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func list() (tms []TimeMachine) {
 	dir := os.TempDir()
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -41,13 +64,28 @@ func find() (tms []TimeMachine) {
 	}
 	for _, file := range files {
 		name := file.Name()
-		if pids := tmDir.FindStringSubmatch(name); pids != nil {
+		if pids := pattern.FindStringSubmatch(name); pids != nil {
 			pid := pids[1]
-			tm := TimeMachine{
-				PID: pid,
-			}
+			tm := get(pid)
 			tms = append(tms, tm)
 		}
 	}
-	return tms
+	return
+}
+
+func get(pid string) (tm TimeMachine) {
+	tm.Pid = pid
+	file := path.Join(os.TempDir(), "timemachine-"+tm.Pid, "get")
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	text := strings.TrimSpace(string(data))
+	tm.Value, err = strconv.ParseInt(text, 10, 64)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	return
 }
