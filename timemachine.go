c4fc167ea1ec375f95da44006ffc5a55b20672e2
Grzegorz Żur <grzegorz.zur@gmail.com>
2018-11-24 22:21:25 +0100

    Specifying offset with units
    Closes #1

--- timemachine.go
+++ timemachine.go
@@ -13,8 +13,16 @@ import (
 type TimeMachine struct {
 	Pid     string
 	Command string
-	Value   int64
+	Value   int
 }
+
+const (
+	second = 1
+	minute = 60 * second
+	hour   = 60 * minute
+	day    = 24 * hour
+	week   = 7 * day
+)
 
 var (
 	pattern = regexp.MustCompile("timemachine-([0-9]+)")
@@ -50,17 +58,19 @@ func get(pid string) (tm TimeMachine) {
 		return
 	}
 	text := string(data)
-	tm.Value, err = strconv.ParseInt(text, 10, 64)
+	value, err := strconv.ParseInt(text, 10, 64)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	tm.Value = int(value)
 	return
 }
 
-func set(pid, value string) {
+func set(pid string, value int) {
 	file := path.Join(os.TempDir(), "timemachine-"+pid, "set")
-	data := []byte(value)
+	text := strconv.Itoa(value)
+	data := []byte(text)
 	err := ioutil.WriteFile(file, data, os.ModeNamedPipe|0775)
 	if err != nil {
 		log.Println(err)
@@ -76,5 +86,30 @@ func command(pid string) (cmd string) {
 		return
 	}
 	cmd = strings.Replace(string(data), "\x00", " ", -1)
+	return
+}
+
+func split(value int) (sign, weeks, days, hours, minutes, seconds int) {
+	if value < 0 {
+		sign = -1
+		value = -value
+	} else {
+		sign = 1
+	}
+	weeks, value = value/week, value%week
+	days, value = value/day, value%day
+	hours, value = value/hour, value%hour
+	minutes, value = value/minute, value%minute
+	seconds = value
+	return
+}
+
+func join(sign, weeks, days, hours, minutes, seconds int) (value int) {
+	value += weeks * week
+	value += days * day
+	value += hours * hour
+	value += minutes * minute
+	value += seconds
+	value *= sign
 	return
 }
--- web.go
+++ web.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 var (
-	tms = template.Must(template.ParseFiles("tms.html"))
-	tm  = template.Must(template.ParseFiles("tm.html"))
+	tmsTmpl = template.Must(template.ParseFiles("tms.html"))
+	tmTmpl  = template.Must(template.ParseFiles("tm.html"))
 )
+
+type Values struct {
+	Pid     string
+	Command string
+	Method  string
+	Value   int
+	Sign    int
+	Weeks   int
+	Days    int
+	Hours   int
+	Minutes int
+	Seconds int
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch {
@@ -23,17 +38,55 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
-	tms.Execute(w, list())
+	tmsTmpl.Execute(w, list())
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	pid := strings.Trim(r.URL.Path, "/")
-	tm.Execute(w, get(pid))
+	tm := get(pid)
+	vs := Values{
+		Pid:     tm.Pid,
+		Command: tm.Command,
+		Value:   tm.Value,
+	}
+	methods := r.URL.Query()["method"]
+	if len(methods) == 1 {
+		vs.Method = methods[0]
+	}
+	vs.Sign, vs.Weeks, vs.Days, vs.Hours, vs.Minutes, vs.Seconds = split(tm.Value)
+	tmTmpl.Execute(w, vs)
 }
 
 func setHandler(w http.ResponseWriter, r *http.Request) {
 	pid := strings.Trim(r.URL.Path, "/")
-	value := r.FormValue("value")
+	method := r.FormValue("method")
+	value := 0
+	switch method {
+	case "raw":
+		value = formInt(r, "value")
+	case "units":
+		sign := formInt(r, "sign")
+		weeks := formInt(r, "weeks")
+		days := formInt(r, "days")
+		hours := formInt(r, "hours")
+		minutes := formInt(r, "minutes")
+		seconds := formInt(r, "seconds")
+		value = join(sign, weeks, days, hours, minutes, seconds)
+	}
 	set(pid, value)
-	http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+	http.Redirect(w, r, r.URL.Path+"?method="+method, http.StatusSeeOther)
+}
+
+func formInt(r *http.Request, name string) (value int) {
+	text := r.FormValue(name)
+	if text == "" {
+		return
+	}
+	v, err := strconv.ParseInt(text, 10, 64)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	value = int(v)
+	return
 }
